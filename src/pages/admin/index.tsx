074ed44aefae2c88980611326effbd5dcd3a59cf
ali <marefati110@gmail.com>
2023-07-03 23:50:41 +0330

    wip

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,10 +1,7 @@
-import Router from 'next/router'
-import React, { useEffect } from 'react'
+import AdminLayout from "layouts/admin";
+import Router from "next/router";
+import React, { useEffect } from "react";
 
-export default function Admin () {
-  useEffect(() => {
-    Router.push('/admin/default')
-  })
-
-  return <div />
+export default function Admin() {
+  return <AdminLayout>ali</AdminLayout>;
 }
